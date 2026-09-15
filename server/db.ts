99d91d8717ebe137e182e060f9725d18b3866722
agustrnt-bit <agustrnt@gmail.com>
2026-09-16 06:47:37 +0700

    feat: add database pool

---
+++ server/db.ts
@@ -0,0 +1,12 @@
+import pg from 'pg';
+import type { QueryResultRow } from 'pg';
+
+const { Pool } = pg;
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export async function query<T extends QueryResultRow = any>(text: string, params: any[] = []) {
+  return pool.query<T>(text, params);
+}
