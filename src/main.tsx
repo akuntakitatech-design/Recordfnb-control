c2b51525d9e6ee628284ce050c502aa6b30a884c
agustrnt-bit <agustrnt@gmail.com>
2026-09-16 10:17:49 +0700

    style: load security settings styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './styles.css';
 import './foundation.css';
 import './searchSelect.css';
 import './financeMaster.css';
+import './settingsSecurity.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
